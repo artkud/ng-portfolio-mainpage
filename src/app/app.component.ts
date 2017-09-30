3182fcf9cdbadf459d0ea7f3ed4b6e57226aecef
artkud <artem.kudroff@gmail.com>
2017-09-30 13:31:00 +0300

    Changed components. Added theme file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,36 +4,14 @@ import 'rxjs/Rx';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  spaceScreens: Array<any>;
   navLinks: Array<any>;
-
   constructor(private http: Http) {
     this.navLinks = [{label: 'Home', href: '/'}, {
       label: 'Portfolio',
       href: '/'
     }, {label: 'About', href: '/'}, {label: 'Contact', href: '/'}];
-
-    this.http.get('./assets/data.json')
-        .map(response => response.json().screenshots)
-        .subscribe(res => this.spaceScreens = res);
-  }
-
-  likeMe(i) {
-    if (this.spaceScreens[i].liked === 0) {
-      this.spaceScreens[i].liked = 1;
-
-    }
-    else {
-      this.spaceScreens[i].liked = 0;
-    }
-
-  }
-
-  deleteMe(i) {
-    this.spaceScreens.splice(i, 1);
-    console.log(i);
   }
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {Http} from '@angular/http';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.less']
+  styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent implements OnInit {
+export class PortfolioComponent {
+  portfolioImages: Array<any>;
 
-  constructor() { }
+  constructor(private http: Http) {
+    this.http.get('./assets/data.json')
+        .map(response => response.json().portfolios)
+        .subscribe(res => this.portfolioImages = res);
+  }
+  likeMe(i) {
+    if (this.portfolioImages[i].liked === 0) {
+      this.portfolioImages[i].liked = 1;
 
-  ngOnInit() {
+    }
+    else {
+      this.portfolioImages[i].liked = 0;
+    }
+
   }
 
+  deleteMe(i) {
+    this.portfolioImages.splice(i, 1);
+    console.log(i);
+  }
 }
